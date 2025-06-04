cbfe70d02e72dfe6b7f2064b5bd9159a9dec4eda
Harika-Rolla <harikarolla@gmail.com>
2025-06-04 12:50:33 +0100

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo "Cleaning old app directory..."
-# Give ec2-user permission to remove everything
+
+# Change ownership to ec2-user so we can delete safely
 sudo chown -R ec2-user:ec2-user /home/ec2-user/acebook
-# Remove everything *except* the scripts directory (to avoid deleting this script)
-find /home/ec2-user/acebook -mindepth 1 ! -path "/home/ec2-user/acebook/scripts/*" -exec rm -rf {} +
+
+# Delete everything inside acebook EXCEPT the scripts folder and its contents
+find /home/ec2-user/acebook -mindepth 1 ! -path "/home/ec2-user/acebook/scripts*" -exec rm -rf {} +
